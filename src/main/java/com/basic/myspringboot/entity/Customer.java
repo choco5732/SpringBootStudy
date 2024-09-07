44a76a280f3e1f76cb8dfb9c85df9fee03de7285
choco5732 <choco5732@gmail.com>
2024-09-07 13:55:34 +0900

    entity class

--- src/main/java/com/basic/myspringboot/entity/Customer.java
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -1,2 +1,20 @@
-package com.basic.myspringboot.entity;public class Customer {
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String customerId;
+
+    @Column
+    private String customerName;
 }
