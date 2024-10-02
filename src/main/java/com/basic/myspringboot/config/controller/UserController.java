ab17760412980903bed2be88a4c515eae126de71
choco5732 <choco5732@gmail.com>
2024-10-02 17:50:23 +0900

    Business Exception

--- src/main/java/com/basic/myspringboot/config/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/config/controller/UserController.java
@@ -1,16 +1,35 @@
 package com.basic.myspringboot.config.controller;
 
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.repository.UserRepository;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor // final로 선언된 변수에 대한 생성자를 자동으로 생겨줌
+@RequestMapping("/api/users")
 public class UserController {
     private final UserRepository userRepository;
 
+    // @RequiredArgsConstructor 이 애노테이션이 아래 역할을 해줌
     // Constructor Injection
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
     }
+
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+    }
+
+    // findById 리턴타입이 Optional이어서 orElseThrow()로 Exception 처리
 
 
 }
